a1a1d83ff5a68c6418596955a0a5fc8f70b15704
johan-STYL <johan@styldesign.se>
2020-10-29 10:15:19 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Link, Route, Switch } from 'react-router-dom';
 import { Nav } from './Nav';
 import { Search } from './Search';
 import { Cardholder } from './Cardholder';
@@ -10,17 +11,19 @@ const api = new Unsplash('apikey');
 function App() {
   const [apiResponse, setApiResponse] = useState({ results: [] });
   return (
-    <div className="app">
-      <Nav />
-      <Search
-        setApiRes={param => {
-          setApiResponse(param);
-        }}
-        api={api}
-      />
-      <Cardholder apiResponse={apiResponse} />
-      <Pagination />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Nav />
+        <Search
+          setApiRes={param => {
+            setApiResponse(param);
+          }}
+          api={api}
+        />
+        <Cardholder apiResponse={apiResponse} />
+        <Pagination />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Nav/Nav.js
+++ src/Nav/Nav.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../main.css';
 import './Nav.css';
 
 const Nav = () => {
-  return <div className="nav">nav</div>;
+  return (
+    <div className="nav">
+      <Link to="/">Home</Link>
+
+      <Link to="/about">About</Link>
+    </div>
+  );
 };
 
 export default Nav;
